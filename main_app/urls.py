5c614f56adf64fe1d2a1aacac35f1b3a123f0215
Gabby <gabzo24@live.com.au>
2024-03-24 17:55:03 +1100

    search users

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-  path('', LoginView.as_view(), name='home'),
-  path('users/profile/', views.profile, name='profile'),
+  path('', LoginView.as_view(), name='login'),
+  path('accounts/login/', LoginView.as_view(), name='login'),
+  path('logout/', LogoutView.as_view(), name='logout'),
+  path('profile/', views.profile, name='profile'),
+  path('profile/<str:username>/', views.profile, name='profile'),
   path('add_platform/', views.add_platform, name='add_platform'),
   path('add_game/', views.add_game, name='add_game'),
   path('accounts/signup/', views.signup, name='signup'),
+  path('search/', views.user_search, name='user_search'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,36 +1,37 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView as BaseLoginView
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import Profile
 
-# Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 @login_required
-def profile(request):
-    user_profile, created = Profile.objects.get_or_create(user=request.user)
+def profile(request, username=None):
+    if username is None:
+        username = request.user.username
+    user_profile = get_object_or_404(Profile, user__username=username)
     return render(request, 'users/profile.html', {'user_profile': user_profile})
 
-
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # Save the user to the db
-      user = form.save()
-      # Automatically log in the new user
-      login(request, user)
-      return redirect('profile')
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+        else:
+            error_message = 'Invalid sign up - try again'
     else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup template
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+        form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 
 @login_required
 def add_game(request):
@@ -45,3 +46,17 @@ def add_platform(request):
         pass
     else:
         return render(request, 'add_platform.html')
+
+@login_required
+def user_search(request):
+    query = request.GET.get('query')
+    users = User.objects.filter(username__icontains=query)
+    return render(request, 'users/search_results.html', {'results': users})
+
+class CustomLoginView(BaseLoginView):
+    redirect_authenticated_user = True
+
+    def form_valid(self, form):
+        user = form.get_user()
+        login(self.request, user)
+        return redirect(reverse('profile', kwargs={'username': user.username}))
