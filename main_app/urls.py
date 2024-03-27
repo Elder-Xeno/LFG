a499e5d5fa25b8eb05ca44165da68e3b9bf6e456
Gabby <gabzo24@live.com.au>
2024-03-28 02:00:03 +1100

    create chat app

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
@@ -12,4 +12,5 @@ urlpatterns = [
   path('add_game/', views.add_game, name='add_game'),
   path('accounts/signup/', views.signup, name='signup'),
   path('search/', views.user_search, name='user_search'),
+  path("chat/", include("chat.urls")),
 ]
